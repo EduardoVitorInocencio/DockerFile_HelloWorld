0654dcc9c229e904a6eb3ae29a959487435e0354
EduardoVitorInocencio <eduardovitor16@gmail.com>
2025-03-29 17:09:00 -0300

    feat: add Redis integration and hit count functionality
    - Integrated Redis for caching with connection to the Redis server.
    - Added get_hit_count function to track and increment hit counts with retry logic.

--- app.py
+++ app.py
@@ -21,8 +21,9 @@ def get_hit_count():
             time.sleep(0.5)
 
 @app.route('/', methods=['GET'])
-def home():
-    return "Hello World!"
+def hello():
+    count = get_hit_count()
+    return "Hello! I've been seen {} times.\n".format(count)
 
 # if __name__ == '__main__':
 #     app.run(debug=True, host="0.0.0.0", port=5000)
