c184275522d06dd4e9eb6bfaae8d350c15495608
EduardoVitorInocencio <eduardovitor16@gmail.com>
2025-03-29 17:02:48 -0300

    feat: add Redis integration and hit count functionality

--- app.py
+++ app.py
@@ -2,13 +2,28 @@
 
 from flask import Flask
 import os
+import time
+import redis
 
 app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            cache.reset_retry_count()
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 @app.route('/', methods=['GET'])
 def home():
     return "Hello World!"
 
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=True, host="0.0.0.0", port=5000)
     
